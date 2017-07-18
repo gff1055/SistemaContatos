924a6e78bf51afc1fe524c9185814b7812be1099
gff1055 <gff105@gmail.com>
2017-07-17 21:09:14 -0300

    Adicao de logica para listagem de contatos

--- src/br/com/caelum/mvc/logica/ListaContatosLogic.java
+++ src/br/com/caelum/mvc/logica/ListaContatosLogic.java
@@ -12,10 +12,13 @@ public class ListaContatosLogic implements Logica {
 	
 	public String executa(HttpServletRequest req, HttpServletResponse res) throws Exception{
 		
+		//MONTA A LISTA DE CONTATOS
 		List<Contato> contatos = new ContatoDao().getLista();
 		
+		//GUARDA A LISTA NO REQUEST
 		req.setAttribute("contatos", contatos);
 		
+		//A LISTA DE CONTATOS EM contatos SERÁ ACESSADA POR EXPRESSION LANGUAGE ${contatos}
 		return "lista-contatos.jsp";
 		
 	}
